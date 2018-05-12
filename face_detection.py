fd63cdd4c7728d22394dd9bc8f0f870e9e5a4e3a
huangwenxi <389125903@qq.com>
2018-05-12 18:22:46 +0800

    1：修复在windows中文件的路径不能包含：的bug 2：修复在windows中路径分割符是\的bug 3：支持用dlib识别人脸，截取人脸部信息 4：在setting.json中增加了配置信息

--- face_detection.py
+++ face_detection.py
@@ -4,6 +4,8 @@ import os
 import json
 import time
 import threading
+import dlib
+from skimage import io
 class DetectFace():
     def __init__(self):
         self.converted_videos= []
@@ -14,7 +16,7 @@ class DetectFace():
         self.all_pictures_num = 0
 
     def creat_cascadeclassifier(self):
-        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.face_cascade = cv2.CascadeClassifier(self.classifier)
 
     def scan_video(self):
         while True:
@@ -52,12 +54,12 @@ class DetectFace():
 
             elif os.path.isfile(file) is True:
                 if file not in self.converted_pictures:
-                    self.detect_picture(file, self.output_path)
+                    self.detect_picture_opencv(file, self.output_path)
                     self.converted_pictures.append(file)
                     self.all_pictures_num += 1
                     print('Conversion rate:' + str(detect.converted_pictures_num / detect.all_pictures_num))
 
-    def detect_picture(self, picture, output_path):
+    def detect_picture_opencv(self, picture, output_path):
         try:
             img = cv2.imread(picture)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -81,12 +83,56 @@ class DetectFace():
                 parname_picture = os.path.basename(os.path.dirname(picture))
                 filepath, tempfilename = os.path.split(picture);
                 (shotname, extension) = os.path.splitext(tempfilename);
-                output_picture = os.path.join(output_path, time.ctime(int(shotname) + int(parname_picture)) + '.jpeg')
+                c_time = time.localtime(int(shotname) + int(parname_picture))
+                output_picture = os.path.join(output_path,  str(c_time.tm_year) + '_' +
+                                              str(c_time.tm_mon) + '-' + str(c_time.tm_mday) + '_' + str(c_time.tm_hour) + '_' +
+                                              str(c_time.tm_min) + '_' + str(c_time.tm_sec) + '.jpeg')
                 print('output:' + output_picture)
                 cv2.imwrite(output_picture, roi_color)
-                print('output... ' + output_picture)
+             
         else:
             print('can not convert picture:' + picture)
+
+    def detect(self, picture, output_path):
+        # 初始化dlib人脸检测器
+        detector = dlib.get_frontal_face_detector()
+
+        # opencv加载视频文件
+        # cap = cv2.VideoCapture('2018-1-1/192.168.1.64_01_20180511172841401_1.mp4')
+
+        index = 1
+        img2 = None
+        # while True:
+            # ret, cv_img = cap.read()
+            # if cv_img is None:
+            #     break
+        try:
+            img = io.imread(picture)
+
+            dets = detector(img, 1)
+            print("Number of faces detected: {}".format(len(dets)))
+            for i, d in enumerate(dets):
+                print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+                    i, d.left(), d.top(), d.right(), d.bottom()))
+                img2 = img[d.top():d.bottom(), d.left():d.right()]
+                parname_picture = os.path.basename(os.path.dirname(picture))
+                filepath, tempfilename = os.path.split(picture);
+                (shotname, extension) = os.path.splitext(tempfilename);
+                c_time = time.localtime(int(shotname) + int(parname_picture))
+                output_picture = os.path.join(output_path, str(c_time.tm_year) + '_' +
+                                              str(c_time.tm_mon) + '-' + str(c_time.tm_mday) + '_' + str(
+                    c_time.tm_hour) + '_' +
+                                              str(c_time.tm_min) + '_' + str(c_time.tm_sec) + '.jpeg')
+
+                io.imsave(output_picture, img2)
+        except:
+            pass
+                # img2 = img[163:271, 28:91]
+                # print(img2)
+                # io.imsave('output/%d.jpeg'%index, img2)
+                # dlib.hit_enter_to_continue()
+
+
 
     def load_setting(self):
         f = open('setting.json', encoding='utf-8')  # use utf-8 encoding to parse file
@@ -100,17 +146,18 @@ class DetectFace():
         self.bit = setting.get('bit')
         self.resolution = setting.get('resolution')
         self.ffmpeg = setting.get('ffmpeg')
+        self.classifier = setting.get('classifier')
         print('resolution' + self.resolution)
 
     def convert_video2_picture(self, video_input_path, output_path):#-vframes 30 -dframes 30
-        ffmpeg_cmd = self.ffmpeg +' -i ' + video_input_path + ' -r ' + str(self.time) + ' -s ' + self.resolution + ' -f image2 '+ output_path +'/%'+ str(self.bit) +'d.jpeg'
+        ffmpeg_cmd = self.ffmpeg +' -i ' + video_input_path + ' -r ' + str(self.time) + ' -s ' + self.resolution + ' -f image2 '+ output_path + os.path.sep + '%'+ str(self.bit) +'d.jpeg'
         os.system(ffmpeg_cmd)
         print('.......................................')
 
 if __name__ == '__main__':
     detect = DetectFace()
-    detect.creat_cascadeclassifier()
     detect.load_setting()
+    detect.creat_cascadeclassifier()
 
     t = threading.Thread(target=detect.scan_video)
     t.start()
