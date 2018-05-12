ab3a8f569f5c31ca81a02802f8bde6bb673dbb23
huangwenxi <389125903@qq.com>
2018-05-12 18:16:58 +0800

    1：利用ffmpeg将视频截取成图片放到输入目录，通过opencv识别图片截取人脸信息存到到输出目录，视频文件的所在目录，ffmpeg截取视频成图片的输出目录，人脸的输出目录等各种配置信息都可以在setting.json中配置 2：人脸图片的名字和在视频中出现的时间是相关的

--- face_detection.py
+++ face_detection.py
@@ -3,34 +3,61 @@ import cv2
 import os
 import json
 import time
+import threading
 class DetectFace():
-    def __init__(self, output_path, rate):
-        self.output_path = output_path
+    def __init__(self):
+        self.converted_videos= []
+        self.Unconverte_videos = {}
         self.converted_pictures = []
         self.Unconverte_pictures = []
         self.converted_pictures_num = 0
         self.all_pictures_num = 0
-        self.rate = rate
 
     def creat_cascadeclassifier(self):
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    def scan_input(self, input_path):
-        if os.path.isdir(input_path) is False:
-            return -1
+    def scan_video(self):
+        while True:
+            time.sleep(10)
+            if os.path.isdir(self.video_input_path) is False:
+                print(self.video_input_path +' not dir ')
+                return -1
+            files = os.listdir(self.video_input_path)
+            print(files)
+            print(self.converted_videos)
+            for _file in files:
+                file = os.path.join(self.video_input_path, _file)
+                if file not in self.converted_videos:
+                    file_ctime = os.stat(file).st_ctime
+                    file_mtime = os.stat(file).st_mtime
+                    print(int(file_ctime))
+                    if time.time() - file_mtime > 30:
+                        print('time.times() - os.stat(file).st_mtime > 30')
+                        video_2_pictures_path = os.path.join(self.video_2_pictures_path, str(int(file_ctime)))
+                        try:
+                            os.mkdir(video_2_pictures_path)
+                            pass
+                        except:
+                            pass
+                        self.convert_video2_picture(file, video_2_pictures_path)
+                        self.converted_videos.append(file)
+
+
+    def scan_picture(self, input_path):
         files = os.listdir(input_path)
         for _file in files:
             file = os.path.join(input_path, _file)
             if os.path.isdir(file) is True:
-                self.scan_input(file)
+                self.scan_picture(file)
 
             elif os.path.isfile(file) is True:
                 if file not in self.converted_pictures:
-                    self.detect_picture(file)
+                    self.detect_picture(file, self.output_path)
                     self.converted_pictures.append(file)
                     self.all_pictures_num += 1
+                    print('Conversion rate:' + str(detect.converted_pictures_num / detect.all_pictures_num))
 
-    def detect_picture(self, picture):
+    def detect_picture(self, picture, output_path):
         try:
             img = cv2.imread(picture)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -41,7 +68,6 @@ class DetectFace():
 
         print('input... ' + picture)
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
-
         if len(faces) != 0:
             self.converted_pictures_num += 1
             img_height = len(img)
@@ -52,36 +78,45 @@ class DetectFace():
                 a_w = min(x, img_width - w -x)*self.rate
                 a_h = min(y, img_height - h -y)*self.rate
                 roi_color = img[int(y - 0.5*a_h):int(y + 0.5*a_h) + h, int(x - 0.5*a_w):int(x + 0.5*a_w) + w]
-                output_picture = os.path.join(self.output_path,os.path.basename(picture))
+                parname_picture = os.path.basename(os.path.dirname(picture))
+                filepath, tempfilename = os.path.split(picture);
+                (shotname, extension) = os.path.splitext(tempfilename);
+                output_picture = os.path.join(output_path, time.ctime(int(shotname) + int(parname_picture)) + '.jpeg')
+                print('output:' + output_picture)
                 cv2.imwrite(output_picture, roi_color)
                 print('output... ' + output_picture)
         else:
-            print('can not convert picture...:' + picture)
+            print('can not convert picture:' + picture)
 
+    def load_setting(self):
+        f = open('setting.json', encoding='utf-8')  # use utf-8 encoding to parse file
+        setting = json.load(f)
 
+        self.video_input_path = setting.get('video_input_path')
+        self.video_2_pictures_path = setting['video_2_pictures_path']
+        self.output_path = setting.get('output_path')
+        self.rate = setting.get('rate')
+        self.time = setting.get('time')
+        self.bit = setting.get('bit')
+        self.resolution = setting.get('resolution')
+        self.ffmpeg = setting.get('ffmpeg')
+        print('resolution' + self.resolution)
 
-def convert_video2_picture(video_path, output_path):#-vframes 30 -dframes 30
-    ffmpeg_cmd = '/anaconda2/envs/py3.6/bin//ffmpeg -i video_file.mkv -r 0.01  -s 640x480 -f image2 '+ output_path +'/video_pictures/%03d.jpeg'
-    os.system(ffmpeg_cmd)
-    print('.......................................')
-
-def load_setting():
-    f = open('setting.json', encoding='utf-8')#use utf-8 encoding to parse file
-    setting = json.load(f)
-    if os.path.isdir(setting['input_path']) is False:
-        print('please config correct input_path in setting.json')
-    if os.path.isdir(setting['output_path']) is False:
-        print('please config correct output_path in setting.json')
-    return setting.get('input_path'),setting.get('output_path'),setting['rate']
-
+    def convert_video2_picture(self, video_input_path, output_path):#-vframes 30 -dframes 30
+        ffmpeg_cmd = self.ffmpeg +' -i ' + video_input_path + ' -r ' + str(self.time) + ' -s ' + self.resolution + ' -f image2 '+ output_path +'/%'+ str(self.bit) +'d.jpeg'
+        os.system(ffmpeg_cmd)
+        print('.......................................')
 
 if __name__ == '__main__':
-    #load setting.json to get 'input_path' and 'output_path'
-    # input_path,output_path,rate = load_setting()
-    # detect = DetectFace(output_path,rate)
-    # detect.creat_cascadeclassifier()
-    # while True:
-    #     detect.scan_input(input_path)
-    #     time.sleep(1)
-    input_path,output_path,rate = load_setting()
-    convert_video2_picture(None, input_path)
+    detect = DetectFace()
+    detect.creat_cascadeclassifier()
+    detect.load_setting()
+
+    t = threading.Thread(target=detect.scan_video)
+    t.start()
+    print('.....................')
+    while True:
+        time.sleep(1)
+        detect.scan_picture(detect.video_2_pictures_path)
+
+
